15ddbfeadc9091ef93838b5487b1f1cf4c22dd5b
ifotn <Rich.Freeman@GeorgianCollege.ca>
2024-09-16 09:53:00 -0400

    l2/3 - choose shop category

--- CheeseAndThankYou/Controllers/ShopController.cs
+++ CheeseAndThankYou/Controllers/ShopController.cs
@@ -8,5 +8,29 @@ namespace CheeseAndThankYou.Controllers
         {
             return View();
         }
+
+        // GET: /Shop/ByCategory/5
+        public IActionResult ByCategory(int id)
+        {
+            switch (id)
+            {
+                case 1:
+                    ViewData["Category"] = "English";
+                    break;
+                case 2:
+                    ViewData["Category"] = "Soft";
+                    break;
+                case 3:
+                    ViewData["Category"] = "Hard";
+                    break;
+                case 4:
+                    ViewData["Category"] = "Blue";
+                    break;
+                default: 
+                    return RedirectToAction("Index");
+            }
+
+            return View();
+        }
     }
 }
