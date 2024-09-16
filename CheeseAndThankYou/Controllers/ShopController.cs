a8e212cb5612d4018ea3cfc10ef39c85d0d11f54
ifotn <Rich.Freeman@GeorgianCollege.ca>
2024-09-16 08:49:13 -0400

    l1/2 - set up shopcontroller

---
+++ CheeseAndThankYou/Controllers/ShopController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CheeseAndThankYou.Controllers
+{
+    public class ShopController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
