c32e84a1cf998782b7038f939916724d812dcc24
ifotn <Rich.Freeman@GeorgianCollege.ca>
2024-09-23 10:41:21 -0400

    l3/2 - complete 1st product form

--- CheeseAndThankYou/Models/Product.cs
+++ CheeseAndThankYou/Models/Product.cs
@@ -1,4 +1,5 @@
 ﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace CheeseAndThankYou.Models
 {
@@ -8,11 +9,16 @@ namespace CheeseAndThankYou.Models
         public string Name { get; set; }
 
         [DisplayName("Stink Rating")]
+        [Range(1, 5)]
         public int StinkRating { get; set; }
 
         public string Description { get; set; }
+
+        [Range(0.01, 1000.00)]
         public decimal Price { get; set; }
-        public string Image { get; set; }
+
+        public string? Image { get; set; }
+        
         public string Size { get; set; }
     }
 }
