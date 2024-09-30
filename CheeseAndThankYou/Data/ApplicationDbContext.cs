a706ecf140c66faa335a48bf4ba2d649e47de736
ifotn <Rich.Freeman@GeorgianCollege.ca>
2024-09-30 09:11:49 -0400

    l5/3 - add DbSets to Data/ApplicationDbContext

--- CheeseAndThankYou/Data/ApplicationDbContext.cs
+++ CheeseAndThankYou/Data/ApplicationDbContext.cs
@@ -10,6 +10,11 @@ namespace CheeseAndThankYou.Data
             : base(options)
         {
         }
-        public DbSet<CheeseAndThankYou.Models.Product> Product { get; set; } = default!;
+        // DbSets based on Model classes.  DbSet names plural, Model names singular
+        public DbSet<Product> Products { get; set; }
+       public DbSet<Category> Categories { get; set; }
+       public DbSet<CartItem> CartItems { get; set; }
+       public DbSet<OrderDetail> OrderDetails { get; set; }
+       public DbSet<Order> Orders { get; set; }
     }
 }
